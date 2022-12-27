ba43e97c748c34764cde1c831cd6e32faeef2edd
Aldrin Morris D. Balitbit <102004801+aldrinbalitbit@users.noreply.github.com>
2022-12-27 19:14:01 +0800

    FIxing CID for container id

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 docker build -t debian-intrap .
 mkdir -p $HOME/live/chroot
-CID=(docker create debian-intrap)
+CID=$(docker create debian-intrap)
 docker export $CID | tar -xf- -C $HOME/live/chroot
