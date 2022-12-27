9635751e37aed2006840f5818433174518b0153f
Aldrin Morris D. Balitbit <102004801+aldrinbalitbit@users.noreply.github.com>
2022-12-27 20:25:26 +0800

    Using rootfs build up

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
-docker build -t debian-intrap .
 mkdir -p $HOME/live/chroot
+echo "Building docker container id..."
+docker build -t debian-intrap . &> /dev/null
 CID=$(docker create debian-intrap)
-docker export $CID | tar -xf- -C $HOME/live/chroot
+echo "Extract the docker container id export..."
+docker export $CID | tar -xf- -C $HOME/live/chroot &> /dev/null
+cd $HOME/live
+mkdir -p $HOME/live/prod/{EFI/boot,boot/grub/x86_64-efi,isolinux,live}
+echo "Compressing chroot and printing filesystem size..."
+mksquashfs chroot prod/live/filesystem.squashfs
+printf $(sudo du -sx --block-size=1 chroot | cut -f1) > prod/live/filesystem.size
