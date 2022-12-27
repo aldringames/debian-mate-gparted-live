e269e654a923ed3113824f2e1800851240e1a6d5
Aldrin Morris D. Balitbit <102004801+aldrinbalitbit@users.noreply.github.com>
2022-12-27 21:05:35 +0800

    List up chroot export

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ docker build -t debian-intrap . &> /dev/null
 CID=$(docker create debian-intrap)
 echo "Extract the docker container id export..."
 docker export $CID | tar -xf- -C $HOME/live/chroot &> /dev/null
+ls $HOME/live/chroot
+apt-get install -qqy squashfs-tools xorriso isolinux syslinux-common grub-pc-bin grub-efi-amd64-bin mtools dosfstools
 cd $HOME/live
-mkdir -p $HOME/live/prod/{EFI/boot,boot/grub/x86_64-efi,isolinux,live}
+mkdir -p $HOME/live/prod/{EFI/boot,boot/grub/x86_64-efi,live}
 echo "Compressing chroot and printing filesystem size..."
-mksquashfs chroot prod/live/filesystem.squashfs
+mksquashfs chroot prod/live/filesystem.squashfs &> /dev/null
 printf $(sudo du -sx --block-size=1 chroot | cut -f1) > prod/live/filesystem.size
