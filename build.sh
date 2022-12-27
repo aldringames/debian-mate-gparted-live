8c8d04be9dc9bfaacccfed4e806e7b2d98c8faa2
Aldrin Morris D. Balitbit <102004801+aldrinbalitbit@users.noreply.github.com>
2022-12-27 19:06:38 +0800

    Remove sudo fixtures

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker build -t debian-intrap .
 mkdir -p $HOME/live/chroot
 CID=(docker create debian-intrap)
-docker export $CID | sudo tar -xf- -C $HOME/live/chroot
+docker export $CID | tar -xf- -C $HOME/live/chroot
