0b4a41d40c7535dce750d49aeb908f26fcef6510
Aldrin Morris D. Balitbit <102004801+aldrinbalitbit@users.noreply.github.com>
2022-12-27 18:20:29 +0800

    Using a run for use

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 sudo mkdir /chroot
 sudo docker build -t debian-intrap .
-sudo docker cp debian-intrap:/* /chroot/
+sudo docker run --name debian-intrap-dist debian-intrap
+sudo docker cp debian-intrap-dist:/* /chroot/
