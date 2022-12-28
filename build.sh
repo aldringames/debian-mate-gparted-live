7675a668e3ccddc58ebcd0f7e9262a1257411017
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2022-12-28 11:51:32 +0800

    Using commit ment asbuild

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ docker export $CID | tar -xf- -C $HOME/live/chroot &> /dev/null
 sudo apt-get install -qqy squashfs-tools xorriso isolinux syslinux-common grub-pc-bin grub-efi-amd64-bin mtools dosfstools
 cd $HOME/live
 mkdir -p $HOME/live/prod/{EFI/boot,boot/grub/x86_64-efi,live}
-touch live/prod/debian
+touch prod/debian
 echo "Compressing filesystem and printing fs size from chroot..."
 mksquashfs chroot prod/live/filesystem.squashfs &> /dev/null
 printf $(sudo du -sx --block-size=1 chroot | cut -f1) > prod/live/filesystem.size
 echo "Converting Arial font to pf2 as grub font..."
 grub-mkfont -o prod/live/arial.pf2 -s 15 $HOME/debian-mate-gparted-live/arial.ttf &> /dev/null
 cp $HOME/debian-mate-gparted-live/splash.png prod/live/
+sudo chroot chroot
